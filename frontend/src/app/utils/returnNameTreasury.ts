e42b2a3320194b539bd223edea3048b549ab999f
TarcisioSilva20 <tarcisior19@hotmail.com>
2025-03-07 20:29:47 -0300

    ajust in function generateNameTreasury

--- frontend/src/app/utils/returnNameTreasury.ts
+++ frontend/src/app/utils/returnNameTreasury.ts
@@ -1,8 +1,6 @@
 import { treasuryType } from "@/types/treasuryType";
 
 export const returnNameTreasury = (array : treasuryType[], id : number) => {
-    console.log("array", array)
-    console.log("id", id)
     const item = array.find(item  => item.id === id)
     return item ? item.name : 'Nao Localizado'
 }
