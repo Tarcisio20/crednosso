bc88e077bb4ffe8652238e4c5e6b4abba1d44369
TarcisioSilva20 <tarcisior19@hotmail.com>
2025-05-12 15:06:12 -0300

    Ajusts in sendEmail

--- backend/src/services/email.ts
+++ backend/src/services/email.ts
@@ -1,18 +1,18 @@
 import transporter from "../utils/emailConfig";
 import { generateEmailTableHTML } from "../utils/generateHtml";
 
-// Função para enviar e-mail automático
 export const sendEmailOfOrder = async (emails: string, orders: any) => {
   const html = generateEmailTableHTML(orders)
   const dt = new Date(orders[0].date).toLocaleDateString("pt-BR", { timeZone: "UTC" })
+  const emailList = emails.split(',').map(email => email.trim());
 
   try {
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to:  'tarcisio.silva@crednosso.com.br,dillan.sousa@crednosso.com.br,luis.lopes@crednosso.com.br,joao.rocha@crednosso.com.br',
-      bbc: emails,
-      subject: `Solicitação de numerario para abastecimento dos terminais Crednosso - ${new Date(orders[0].date).toLocaleDateString("pt-BR", { timeZone: "UTC" })}`,
-      html: `
+    for (const email of emailList) {
+      await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: email+',tarcisio.silva@crednosso.com.br,dillan.sousa@crednosso.com.br,luis.lopes@crednosso.com.br,joao.rocha@crednosso.com.br',
+        subject: `Solicitação de numerario para abastecimento dos terminais Crednosso - ${new Date(orders[0].date).toLocaleDateString("pt-BR", { timeZone: "UTC" })}`,
+        html: `
       Prezados, <br><br><br>
 
       Segue valores solicitados para abastecimentos dos terminais Crednosso.
@@ -25,11 +25,10 @@ export const sendEmailOfOrder = async (emails: string, orders: any) => {
       <br><br><br>
       Atenciosamente,
       `,
-    });
+      });
+    }
     return true
-    //  console.log("E-mail de boas-vindas enviado!");
   } catch (error) {
     return error
-    // console.error("Falha ao enviar e-mail:", error);
   }
 }
